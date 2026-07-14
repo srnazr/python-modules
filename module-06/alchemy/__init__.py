b9af5e9939c78e71b68d3ae13c51c6e23902147e
Serena <serenazaarour@gmail.com>
2026-07-14 20:35:43 +0300

    feat: part 1

---
+++ module-06/alchemy/__init__.py
@@ -0,0 +1,3 @@
+from .elements import create_air
+
+create_air()
---
+++ module-06/ft_alembic_3.py
@@ -0,0 +1,5 @@
+from alchemy.elements import create_air
+
+print("=== Alembic 3 ===")
+print("Accesing alchemy/elements.py using 'from ... import ...' structure")
+print("Testing create_air: ", create_air())
