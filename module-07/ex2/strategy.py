194ec983e8c88a34bc615d8b4d0af690bc067788
Serena <serenazaarour@gmail.com>
2026-07-17 23:17:01 +0300

    feat: implemented strategies

---
+++ module-07/ex2/strategy.py
@@ -0,0 +1,67 @@
+from abc import ABC, abstractmethod
+from ex0.creature import Creature
+from ex1.capability import HealCapability
+from ex1.capability import TransformCapability
+from .exception import InvalidStrategyError
+
+
+class BattleStrategy(ABC):
+    strategy_name = "battle"
+
+    @abstractmethod
+    def is_valid(self, creature):
+        pass
+
+    @abstractmethod
+    def act(self, creature):
+        pass
+
+    def check_valid(self, creature):
+        if self.is_valid(creature) == False:
+            message = (f"Invalid Creature '{creature._name}' for this"
+                       f"{self.strategy_name} strategy" )
+            raise InvalidStrategyError(message)
+
+
+class NormalStrategy(BattleStrategy):
+    strategy_name = "normal"
+
+    def is_valid(self, creature):
+        if isinstance(creature, Creature):
+            return True
+        return False
+    
+    def act(self, creature):
+        self.check_valid(creature)
+        print(creature.attack())
+
+
+class AggressiveStrategy(BattleStrategy):
+
+    strategy_name = "agressive"
+
+    def is_valid(self, creature):
+        if isinstance(creature, TransformCapability):
+            return True
+        return False
+    
+    def act(self, creature):
+        self.check_valid(creature)
+        print(creature.transform())
+        print(creature.attack())
+        print(creature.revert())
+
+
+class DefensiveStrategy(BattleStrategy):
+
+    strategy_name = "defensive"
+
+    def is_valid(self, creature):
+        if isinstance(creature, HealCapability):
+            return True
+        return False
+    
+    def act(self, creature):
+        self.check_valid(creature)
+        print(creature.attack())
+        print(creature.heal())
