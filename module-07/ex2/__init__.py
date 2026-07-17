e831d49e640040b522b092cac7d9a585eee9d586
Serena <serenazaarour@gmail.com>
2026-07-17 23:17:20 +0300

    feat: bundled files into a module

---
+++ module-07/ex2/__init__.py
@@ -0,0 +1,10 @@
+from .strategy import BattleStrategy
+from .strategy import NormalStrategy
+from .strategy import AggressiveStrategy
+from .strategy import DefensiveStrategy
+from .exception import InvalidStrategyError
+
+NormalStrategy()
+AggressiveStrategy()
+DefensiveStrategy()
+InvalidStrategyError()
